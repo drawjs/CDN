03a46e0de1b65e36c9ba3aa64f97a69ed9488afe
TerrySu <2684759855@qq.com>
2018-09-09 16:42:00 +0800

    update

--- iframes/drawExamples/sharedData.js
+++ iframes/drawExamples/sharedData.js
@@ -2,6 +2,7 @@ function codeViewerCallback() {
   return {
     html: [
       '<canvas id="myCanvas"></canvas>',
+      '',
       "<!--Dependencies-->",
       `<script src="https://drawjs.github.io/CDN/iframes/drawExamples/common.js"></script>'`
     ].join( "\n" )
